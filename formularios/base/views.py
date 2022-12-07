b54bd18f0519d98c2a5e7d0e62a90d82eaa5efd1
robertolt <rl.beto.lorenzoni@gmail.com>
2022-12-07 17:49:00 -0300

    implementada submit dos dados mas sem tratamento

--- formularios/base/views.py
+++ formularios/base/views.py
@@ -7,17 +7,42 @@ from .models import Questionario, Pergunta
 
 # Create your views here.
 def home(request):
-    if request.method == 'POST':
-        nunca = request.POST.get('0 Nunca', None)
-        raramente = request.POST.get('1 Raramente', None)
-        ocasionalmente = request.POST.get('2 Ocasionalmente', None)
-        frequentemente = request.POST.get('3 Frequentemente', None)
-        sempre = request.POST.get('4 Sempre', None)
-        print(nunca, raramente, ocasionalmente, frequentemente, sempre)
     q = Questionario.objects.first()
     p = Pergunta.objects.filter(questionario=q).all()
     context = {
         'questionario': q,
         'perguntas': p,
     }
+    erros = {}
+    if request.method == 'POST':
+        name = request.POST
+        print(name)
+        print(p)
+
+
+    #     for  in p.id:
+    #         contador = 0
+    #         nunca = request.POST.get('0 Nunca', None)
+    #         if nunca is None:
+    #             contador += 1
+    #         raramente = request.POST.get('1 Raramente', None)
+    #         if raramente is None:
+    #             contador += 1
+    #         frequentemente = request.POST.get('3 Frequentemente', None)
+    #         if frequentemente is None:
+    #             contador += 1
+    #         ocasionalmente = request.POST.get('2 Ocasionalmente', None)
+    #         if ocasionalmente is None:
+    #             contador += 1
+    #         sempre = request.POST.get('4 Sempre', None)
+    #         if sempre is None:
+    #             contador += 1
+    #         if contador == 4:
+    #             print(nunca, raramente, ocasionalmente, frequentemente, sempre)
+    #         else:
+    #             erros['e'] = 'Favor selecionar uma opção!'
+    # if erros:
+    #     context['erros'] = erros
+    # else:
+    #     pass
     return render(request, 'base/home.html', context=context)
